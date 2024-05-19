ae2ce58ed59caa3b84cb4072c6d55a49028c261a
tufusa <tufusa74@gmail.com>
2024-05-19 15:37:32 +0900

    feat: フェードインを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Door } from "./components/Door";
 
 export const App = () => {
   const [room, setRoom] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+  const fadeIn = useCallback(
+    () =>
+      ref.current?.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 300 }),
+    [ref]
+  );
   const onClick = () => {
     setRoom(Math.floor(Math.random() * 100 + 1));
+    fadeIn();
   };
+
+  useEffect(() => {
+    const animation = fadeIn();
+    return () => animation?.cancel();
+  }, [fadeIn]);
 
   return (
     <div
       className="
-        flex h-screen
-        w-screen flex-row items-center
-        justify-center gap-12
+        flex
+        h-screen w-screen
+        flex-row items-center
+        justify-center
+        gap-12
         bg-white
       "
+      ref={ref}
     >
       <Door onClick={onClick} />
       <Door onClick={onClick} />
